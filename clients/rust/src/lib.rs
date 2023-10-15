22836b8093958c27fe4044a71e0e5942c0b2a613
Felix Ippolitov <fellu@ancient.fi>
2023-10-16 02:21:05 +0300

    nod

--- clients/rust/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod generated;
-pub mod hooked;
-mod traits;
-pub mod utils;
-
-pub use generated::programs::MPL_TOKEN_METADATA_ID as ID;
-pub use generated::*;
--- clients/rust/src/utils.rs
+++
@@ -1,4 +0,0 @@
-/// Removes all null bytes from a string.
-pub fn clean(value: String) -> String {
-    value.replace('\0', "")
-}
